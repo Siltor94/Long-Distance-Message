917ad54b905d5350062dea509eef32188f98082a
antoineMo <moriss_a@etna-alternance.net>
2017-06-24 18:02:33 +0200

    got the contacts list all right

--- LDM-app/src/pages/home/home.ts
+++ LDM-app/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Contacts } from '@ionic-native/contacts';
 import * as io from 'socket.io-client';
 
 
@@ -11,15 +12,51 @@ export class HomePage {
   socket:any
   chat_input:string;
   chats = [];
+  contacts = []
+  groupedContacts = []
 
-  constructor(public navCtrl: NavController) {
-    this.socket = io('http://localhost:3000');
+  constructor(public navCtrl: NavController, public contact: Contacts) {
+    contact.find(["displayName", "phoneNumbers"], {multiple: true, hasPhoneNumber: true}).then((contacts) => {
 
-    this.socket.on('message', (msg) => {
-      console.log("message", msg);
-      this.chats.push(msg);
-    });
-  }
+    for (var i=0 ; i < contacts.length; i++){
+     if(contacts[i].displayName !== null){
+       var obj = {};
+       obj["name"] = contacts[i].displayName;
+       obj["number"] = contacts[i].phoneNumbers[0].value;
+       this.contacts.push(obj)    // adding in separate array with keys: name, number
+     }      
+    }
+
+    this.groupContacts(this.contacts);
+  })
+}
+
+groupContacts(contacts){
+
+  let sortedContacts = contacts.sort(function(a, b){
+    if(String(a.name).toLowerCase() < String(b.name).toLowerCase()) return -1;
+    if(String(a.name).toLowerCase() > String(b.name).toLowerCase()) return 1;
+    return 0;
+   });
+
+  let currentLetter:String = null;
+  let currentContacts = [];
+
+  sortedContacts.forEach((value, index) => {
+    if(String(value.name.charAt(0)).toLowerCase() != currentLetter){
+      currentLetter = String(value.name.charAt(0)).toLowerCase();
+
+      let newGroup ={
+        letter: String(currentLetter).toLowerCase(),
+        contacts:[]
+      };
+
+      currentContacts = newGroup.contacts;
+      this.groupedContacts.push(newGroup);
+    }
+    currentContacts.push(value);
+  });
+}
 
   send(msg) {
         if(msg != ''){
